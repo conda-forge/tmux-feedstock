fd9879b2a2a1ded74438e948c80cd5c093b796dc
Dougal J. Sutherland <dougal@gmail.com>
2018-08-17 13:50:34 +0100

    try dropping CFLAGS etc...maybe that works now?

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Needed for Linux build to work.
-export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
-export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-
 ./configure --prefix=$PREFIX
 make
 make install
