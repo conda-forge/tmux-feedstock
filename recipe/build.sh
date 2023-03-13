8153e035e183ba660070763fc78c56be19a8e308
Matthias Diener <mdiener@illinois.edu>
2023-03-12 23:29:49 -0500

    update to 3.3a

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
+set -ex
+
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./etc
 
 autoreconf -f -i
+
 if [[ ${target_platform} =~ linux.* ]]; then
   ./configure --prefix=$PREFIX
 else
   ./configure --prefix=$PREFIX --enable-utf8proc
 fi
+
 make
 make install
