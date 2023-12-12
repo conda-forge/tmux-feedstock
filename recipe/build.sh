8a471a14752228bfe011de0db187604b5b8b81d0
smautner <smautner@users.noreply.github.com>
2023-12-12 23:47:27 +0100

    no reason not to enable sixel
    enables sixel support

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,9 +8,9 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./etc
 autoreconf -f -i
 
 if [[ ${target_platform} =~ linux.* ]]; then
-  ./configure --prefix=$PREFIX
+  ./configure --prefix=$PREFIX --enable-sixel .
 else
-  ./configure --prefix=$PREFIX --enable-utf8proc
+  ./configure --prefix=$PREFIX --enable-utf8proc --enable-sixel .
 fi
 
 make
